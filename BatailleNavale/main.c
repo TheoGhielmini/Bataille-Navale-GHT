bac99a94febfac3871650108a6d9616d1d94bb59
TheoGhielmini <Theo.GHIELMINI@cpnv.ch>
2021-12-06 15:54:44 +0100

    Commit du 06.12.21

--- BatailleNavale/main.c
+++ BatailleNavale/main.c
@@ -4,23 +4,27 @@
 
 void menu_principale();
 void menu_aide();
+void init_tableau();
+void affiche_tableau();
 
 void menu_aide() {
+    printf("==============================\n");
     printf("Regles du jeu :");
-    printf("\nLes bateaux : \nIl y a cinq bateaux disponible :\nUn zodiaque de 1 case\n"
+    printf("\nLes bateaux : \n\nIl y a cinq bateaux disponible :\n\nUn zodiaque de 1 case\n"
            "Une vedette rapide de 2 cases\n"
            "Un croiseur de 3 cases\n"
            "Un cuirasse de 4 cases\n"
            "Un porte-avion de 5 cases\n\nDeux bateaux ne peuvent pas se toucher\n"
            "Un bateau ne peut pas etre en contact avec le bord de la grille par plus que 1 case");
-    menu_principale();
+    printf("\n==============================");
 }
 
 void menu_principale() {
     int votre_choix;
-    char votre_nom;
+    char votre_nom = "";
     int plateauJeu;
     int debut_partie;
+    int grille[MAX_TAB][MAX_TAB];
     printf("Welcome to BatNav");
     printf("\n=========================\n");
     printf("Que voulez-vous faire?\n");
@@ -38,18 +42,15 @@ void menu_principale() {
 
     switch (votre_choix) {
         case 1:
-            printf("Regles du jeu :");
-            printf("\nLes bateaux : \nIl y a cinq bateaux disponible :\nUn zodiaque de 1 case\n"
-                   "Une vedette rapide de 2 cases\n"
-                   "Un croiseur de 3 cases\n"
-                   "Un cuirasse de 4 cases\n"
-                   "Un porte-avion de 5 cases\n\nDeux bateaux ne peuvent pas se toucher\n"
-                   "Un bateau ne peut pas etre en contact avec le bord de la grille par plus que 1 case");
+            menu_aide();
             break;
 
             case 2:
-                printf("Votre nom : ");
-                scanf("%c", &votre_nom);
+                printf("Votre nom : \n");
+                scanf("%c \n", &votre_nom);
+                printf("Joueur : %c", votre_nom);
+                //init_tableau(grille);
+                //affiche_tableau(grille);
                 break;
 
                 case 3:
@@ -61,8 +62,26 @@ void menu_principale() {
 
 }
 
+void init_tableau(int plateau[MAX_TAB][MAX_TAB]) {
+    for (int x = 0; x < MAX_TAB; x++) {
+        for (int y = 0; y < MAX_TAB; y++) {
+            plateau[x][y] = 0;
+        }
+    }
+}
+
+void affiche_tableau(int plateau[MAX_TAB][MAX_TAB]) {
+    for (int x = 0; x < MAX_TAB; x++) {
+        for (int y = 0; y < MAX_TAB; y++) {
+            printf("%3d", plateau[x][y]);
+        }
+        printf("\n");
+    }
+}
+
 int main() {
     menu_principale();
+
 
 
     return 0;
